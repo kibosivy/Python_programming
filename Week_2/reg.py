c0b6a207878aa38fb4595258494728f1272ce818
kibosivy <kibosivy@gmail.com>
2025-03-31 09:01:55 +0300

    Added assignment

--- Week_2/reg.py
+++ Week_2/reg.py
@@ -1,4 +1,10 @@
-# import re
+# re.findall(pattern, string): Returns a list of all matches in the string
+# re.search(pattern, string): Returns a match object if the pattern is found, otherwise None
+# re.split(pattern, string): Splits the string at each match of the pattern
+# re.sub(pattern, replacement, string): Replaces all matches of the pattern with the replacement string
+# re.compile(pattern): Compiles the regex pattern for later use
+
+import re
 
 text = "We are learning regular expressions in Python"
 
@@ -20,34 +26,34 @@ else:
 # * - match 0 or repetitions
 
 text = "accb a1b aab axb"
-pattern = "a*b"
+pattern = "a.b"
 
 import re
 matches = re.findall(pattern, text)
 print(matches)
 
-text = "Hello World"
-pattern = r"World$"
+# text = "Hello World"
+# pattern = r"World$"
 
-matches = re.findall(pattern, text)
-print(matches)
+# matches = re.findall(pattern, text)
+# print(matches)
 
-match = re.search(pattern,text)
-print(match.group())
+# match = re.search(pattern,text)
+# print(match.group())
 
 
-# character classes
-# [abc]
-# [a-z] - any lowercase character
-# [A-Z] - any uppercase
-# [0-9] - any digit
-# [a-zA-Z0-9] -alphanumeric
-# \d - any digit
-#  \w - any word
-# \s - whitespace
+# # character classes
+# # [abc]
+# # [a-z] - any lowercase character
+# # [A-Z] - any uppercase
+# # [0-9] - any digit
+# # [a-zA-Z0-9] -alphanumeric
+# # \d - any digit
+# #  \w - any word
+# # \s - whitespace
 
-text = "My pone number is 123-456-7890"
-pattern = r"\d+"
+# text = "My pone number is 123-456-7890"
+# pattern = r"\d+"
 
-matches = re.findall(pattern,text)
-print(matches)
+# matches = re.findall(pattern,text)
+# print(matches)
